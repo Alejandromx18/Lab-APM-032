c89aca4ffcaef12895da90d7e14ca676b9ef5c6b
Alejandromx18 <jr825917@gmail.com>
2024-02-28 22:17:30 -0600

    Practica 5

---
+++ basic-ionic/src/app/detalle-receta/detalle-receta.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Receta } from '../interface/receta';
+import { RecetaService } from '../service/receta.service';
+
+@Component({
+  selector: 'app-detalle-receta',
+  templateUrl: './detalle-receta.page.html',
+  styleUrls: ['./detalle-receta.page.scss'],
+})
+export class DetalleRecetaPage implements OnInit {
+
+  idReceta: number = 0;
+  receta: any;
+
+  constructor(
+    private recetaService: RecetaService,
+    private router: Router,
+    private activateRouter: ActivatedRoute) { 
+      this.recetaService = recetaService;
+      this.router = router;
+      this.activateRouter = activateRouter;
+
+    }
+
+  ngOnInit() {
+    this.activateRouter.paramMap.subscribe((paramMap: any)=>{
+      this.idReceta = Number.parseInt(paramMap.get('idReceta'));
+      this.receta = this.recetaService.getReceta(this.idReceta);
+    });
+  }
+
+}
